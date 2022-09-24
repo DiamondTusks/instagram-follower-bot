52e5f6c493297b38f418eddf01dede9f6544265a
Diamond Tusks <90947391+DiamondTusks@users.noreply.github.com>
2022-09-24 21:36:49 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import ElementClickInterceptedException
 import time
 
 CHROME_DRIVER_PATH = "/Users/me/Documents/development/chromedriver"
@@ -28,7 +29,7 @@ class InstaFollower:
         password_field.send_keys(Keys.ENTER)
 
         time.sleep(3)
-        save_info = self.driver.find_element(By.CLASS_NAME, "sqdOP")
+        save_info = self.driver.find_element(By.CLASS_NAME, 'o9w3sbdw')
         save_info.click()
 
         time.sleep(3)
@@ -40,20 +41,27 @@ class InstaFollower:
         self.driver.get(f"https://www.instagram.com/{SIMILAR_ACCOUNT}/")
 
         time.sleep(3)
-        followers = self.driver.find_element(By.CLASS_NAME, 'qi72231t')
+        followers = self.driver.find_element(By.XPATH, '//*[@id="mount_0_0_EN"]/div/div/div/div[1]/div/div/div/div[1]/section/main/div/div/div/div/button')
         followers.click()
 
         time.sleep(3)
         # follower_popup = self.driver.find_element(By.CLASS_NAME, '_anno')
-        scrollable_popup = self.driver.find_element(By.XPATH, '')
-        for i in range(5):
+        scrollable_popup = self.driver.find_element(By.XPATH, '/html/body/div[4]/div/div/div[2]')
+        for i in range(10):
             self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", scrollable_popup)
             time.sleep(2)
 
 
 
     def follow(self):
-        pass
+        all_buttons = self.driver.find_elements(By.CSS_SELECTOR, "li button")
+        for button in all_buttons:
+            try:
+                button.click()
+                time.sleep(1)
+            except ElementClickInterceptedException:
+                cancel_button = self.driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div/div[3]/button[2]')
+                cancel_button.click()
 
 insta_follower = InstaFollower(SERVICE)
 insta_follower.login()
